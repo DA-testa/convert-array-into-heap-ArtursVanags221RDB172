904419c926d82f052a03c0809e4704136b33d803
ArtursVanags221RDB172 <124793162+ArtursVanags221RDB172@users.noreply.github.com>
2023-03-18 19:26:55 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -3,75 +3,40 @@
 
 def build_heap(data):
     swaps = []
+    # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
-    n = len(data)
-    for i in range(n// 2, -1, -1):
-
-        smth=i
-        right = 2*smth+2 
-        left = 2*smth+1
-
-        while True:
-            index = smth
-            if right < n and data[right] < data[index]:
-                index = right
-            if left < n and data[left] < data[index]:
-                index=left
-
-            if smth != index:
-                swaps.append((smth, index))
-                data[smth], data[index] = data[index], data[smth]
-                smth = index
-                right= 2*smth + 2
-                left= 2*smth + 1
-            
-            else:
-                break
 
 
     return swaps
 
 
 def main():
-    apainter = input()
-
-    if "F" in apainter:
-        path = './test/'
-        file = input()
-        folder = path + file
-        if "a" not in file:
-            try:
-                with open(folder) as f:
-                    n = int(f.readline())
-                    data = list(map(int, f.readline().split()))
-            except Exception as e:
-                print("Kluda:(", str(e))
-                return
-
-        else:
-            print("Kluda")
-
-    if "I" in apainter:
-        n = int(input())
-        data = list(map(int, input().split()))
-
-    # Check that data is assigned before use
     
+    # TODO : add input and corresponding checks
+    # add another input for I or F 
+    # first two tests are from keyboard, third test is from a file
 
-    # checks if length of data is the same as the said length
+
+    # input from keyboard
+    n = int(input())
+    data = list(map(int, input().split()))
+
+    # checks if lenght of data is the same as the said lenght
     assert len(data) == n
 
-    # calls function to assess the data
+    # calls function to assess the data 
     # and give back all swaps
     swaps = build_heap(data)
 
+    # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
-    assert len(swaps) <= n * 4
+
 
     # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
+
 
 if __name__ == "__main__":
     main()
